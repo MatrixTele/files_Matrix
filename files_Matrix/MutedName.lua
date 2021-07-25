8c844c6728e724547c1b4b504aa866a9609b23f1
MatrixTele <80210649+MatrixTele@users.noreply.github.com>
2021-07-25 17:50:51 +0300

    Add files via upload

--- files_Matrix/MutedName.lua
+++ files_Matrix/MutedName.lua
@@ -9,39 +9,39 @@ user_id_ = user_id
 end
 if text and text:match("^كتم اسم (.*)$") and Owner(msg) and database:get(bot_id.."block:name:stats"..msg.chat_id_) == "open" then
 local BlNe = text:match("^كتم اسم (.*)$")
-send(msg.chat_id_, msg.id_, '≁⎠ تم كتم الاسم '..BlNe)
+send(msg.chat_id_, msg.id_, '⌔︙تم كتم الاسم '..BlNe)
 database:sadd(bot_id.."Matrix:blocname"..msg.chat_id_, BlNe)
 end
 
 if text and text:match("^الغاء كتم اسم (.*)$") and Owner(msg) and database:get(bot_id.."block:name:stats"..msg.chat_id_) == "open" then
 local delBn = text:match("^الغاء كتم اسم (.*)$")
-send(msg.chat_id_, msg.id_, '≁⎠ تم الغاء كتم الاسم '..delBn)
+send(msg.chat_id_, msg.id_, '⌔︙تم الغاء كتم الاسم '..delBn)
 database:srem(bot_id.."Matrix:blocname"..msg.chat_id_, delBn)
 end
 
 if text == "مسح الاسماء المكتومه" and Constructor(msg) and database:get(bot_id.."block:name:stats"..msg.chat_id_) == "open" then
 database:del(bot_id.."Matrix:blocname"..msg.chat_id_)
-texts = "≁⎠  تم مسح الاسماء المكتومه "
+texts = "⌔︙ تم مسح الاسماء المكتومه "
 send(msg.chat_id_, msg.id_, texts)
 end
 if text == "الاسماء المكتومه" and Constructor(msg) and database:get(bot_id.."block:name:stats"..msg.chat_id_) == "open" then
 local All_name = database:smembers(bot_id.."Matrix:blocname"..msg.chat_id_)
-t = "\n≁⎠ قائمة الاسماء المكتومه \n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ \n"
+t = "\n⌔︙قائمة الاسماء المكتومه \n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ \n"
 for k,v in pairs(All_name) do
 t = t..""..k.."- (["..v.."])\n"
 end
 if #All_name == 0 then
-t = "≁⎠ لا يوجد اسماء مكتومه"
+t = "⌔︙لا يوجد اسماء مكتومه"
 end
 send(msg.chat_id_, msg.id_, t)
 end
 end
 if text == "تفعيل كتم الاسم" and Constructor(msg) then
-send(msg.chat_id_, msg.id_, '≁⎠ تم التفعيل سيتم كتم العضو الذي يضع الاسماء المكتومه')
+send(msg.chat_id_, msg.id_, '⌔︙تم التفعيل سيتم كتم العضو الذي يضع الاسماء المكتومه')
 database:set(bot_id.."block:name:stats"..msg.chat_id_,"open")
 end
 if text == "تعطيل كتم الاسم" and Constructor(msg) then
-send(msg.chat_id_, msg.id_, '≁⎠ تم تعطيل سيتم كتم العضو الذي يضع الاسماء المكتومه')
+send(msg.chat_id_, msg.id_, '⌔︙تم تعطيل سيتم كتم العضو الذي يضع الاسماء المكتومه')
 database:set(bot_id.."block:name:stats"..msg.chat_id_,"close")
 end
 if not Owner(msg) and database:get(bot_id.."block:name:stats"..msg.chat_id_) == "open" then
--- files_Matrix/SharGroup.lua
+++ files_Matrix/SharGroup.lua
@@ -2,19 +2,19 @@ local function Shaar(msg)
 local text = msg.content_.text_
 if text then
 if text == 'تفعيل تنبيه الاشعار' and Owner(msg) then
-send(msg.chat_id_, msg.id_,'*≁⎠ تم تفعيل التنبيه -› الاشعار !*')
+send(msg.chat_id_, msg.id_,'*⌔︙تم تفعيل التنبيه -› الاشعار !*')
 database:set(bot_id..'Matrix:SH:BOT'..msg.chat_id_,true)
 return false
 end
 if text == 'تعطيل تنبيه الاشعار' and Owner(msg) then
-send(msg.chat_id_, msg.id_,'*≁⎠ تم تعطيل التنبيه -› الاشعار !*')
+send(msg.chat_id_, msg.id_,'*⌔︙تم تعطيل التنبيه -› الاشعار !*')
 database:del(bot_id..'Matrix:SH:BOT'..msg.chat_id_)
 return false
 end
 if text and text:match('^وضع الشعار (.*)$') and Owner(msg) then
 local SHGP = text:match('^وضع الشعار (.*)$')
 database:set(bot_id..'Matrix:SH:BOT:GP'..msg.chat_id_,SHGP)
-send(msg.chat_id_, msg.id_,'≁⎠ تم حفظ شعار المجموعه \n')
+send(msg.chat_id_, msg.id_,'⌔︙تم حفظ شعار المجموعه \n')
 end
 end
 if text and database:get(bot_id..'Matrix:SH:BOT'..msg.chat_id_) and not Owner(msg) then
@@ -31,11 +31,11 @@ local taha1 = (database:get(bot_id..'Matrix:SH:BOT:GP'..msg.chat_id_) or '')
 if string.find(taha,taha1) == nil then
 local taha = tonumber((database:get(bot_id..'Matrix:SH:NUM'..msg.chat_id_..msg.sender_user_id_) or 0))
 if taha == 1 then
-send(msg.chat_id_, msg.id_,'\n≁⎠ عليك وضع شعار المجموعه لديك {3} محاولات وعند انتهاء المحاولات سيتم طردك الشعار {`'..taha1..'`}')
+send(msg.chat_id_, msg.id_,'\n⌔︙عليك وضع شعار المجموعه لديك {3} محاولات وعند انتهاء المحاولات سيتم طردك الشعار {`'..taha1..'`}')
 elseif taha == 2 then
-send(msg.chat_id_, msg.id_,'\n≁⎠ عليك وضع شعار المجموعه لديك {2} محاولات وعند انتهاء المحاولات سيتم طردك الشعار {`'..taha1..'`}')
+send(msg.chat_id_, msg.id_,'\n⌔︙عليك وضع شعار المجموعه لديك {2} محاولات وعند انتهاء المحاولات سيتم طردك الشعار {`'..taha1..'`}')
 elseif taha == 3 then
-send(msg.chat_id_, msg.id_,'\n≁⎠ عليك وضع شعار المجموعه لديك اخر محاولات لوضع الشعار {`'..taha1..'`}')
+send(msg.chat_id_, msg.id_,'\n⌔︙عليك وضع شعار المجموعه لديك اخر محاولات لوضع الشعار {`'..taha1..'`}')
 end
 database:incrby(bot_id..'Matrix:SH:NUM'..msg.chat_id_..msg.sender_user_id_,1)
 end
